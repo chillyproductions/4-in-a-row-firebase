865b4207fb78e8dda136bf1e17b0c111eb84ce84
chillyproductions <54706474+chillyproductions@users.noreply.github.com>
2020-11-06 18:09:27 +0200

    11.6

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -2,10 +2,10 @@ addEventListener('load', () =>{
     const db = firebase.firestore();
     const users = db.collection("users").orderBy("wins", "desc").limit(10);
     var s="";
-    users.forEach(user =>{
-        user.get().then((doc) =>{
-            console.log(doc.data().name)
-        })
+    users.get().then((docs)=>{
+        docs.docs.forEach(doc => {
+            console.log(doc.data());
+        });
     })
 
 })
